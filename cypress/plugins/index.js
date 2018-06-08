86a6b5901cc964dbf413d204ac4a645d8bcc591e
miklos <m.miklos05@gmail.com>
2018-06-08 22:07:04 +0200

    upgrade cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,7 +2,7 @@ const webpack = require('@cypress/webpack-preprocessor')
 
 module.exports = (on) => {
     const options = webpack.defaultOptions
-    // options.webpackOptions.module.rules[0].use[0].options.plugins = [['../../src/index.js', { __sinker: '../../src/sinker/index.js' }]]
+    options.webpackOptions.module.rules[0].use[0].options.plugins = [['../../src/index.js', { __sinker: '../../src/sinker.js' }]]
 
     on('file:preprocessor', webpack(options))
 }
--- src/sinker.js
+++ src/sinker.js
@@ -5,19 +5,18 @@ const TESTID = 'data-test';
 const addTestID = (element, testID) => React.cloneElement(element, {[TESTID]: testID});
 
 module.exports = function sinker(element, testID) {
-    return element;
-    // if (typeof element.type === 'string') {
-    //     return addTestID(element, testID);
-    // }
-    // let found = false;
-    // return React.Children.map(element.children, child => {
-    //     if (found) {
-    //         return child;
-    //     }
-    //     if (typeof child.type === 'string') {
-    //         found = true;
-    //         return addTestID(element, testID);
-    //     }
-    //     return sinker(child, testID)
-    // });
+    if (typeof element.type === 'string') {
+        return addTestID(element, testID);
+    }
+    let found = false;
+    return React.Children.map(element.children, child => {
+        if (found) {
+            return child;
+        }
+        if (typeof child.type === 'string') {
+            found = true;
+            return addTestID(element, testID);
+        }
+        return sinker(child, testID)
+    });
 };
